e6202e035a1591a0cac6d936e3e436db40ef03ca
浅墨 <happylifemxy@163.com>
2018-01-21 15:06:58 +0800

    Initial commit

---
+++ Source/UE4BuildingEscape/UE4BuildingEscapeGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "UE4BuildingEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4BUILDINGESCAPE_API AUE4BuildingEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
